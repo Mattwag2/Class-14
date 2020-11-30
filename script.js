d6e9ffc37b24261fc557c2d3949f0d00d338c056
Matt Wagner <mattwagner@Matts-MacBook-Air.local>
2020-11-29 23:38:21 -0600

    Changes made

--- script.js
+++ script.js
@@ -8,6 +8,18 @@ function init () {
     paragraph = document.querySelector("p");
 
     // Put your code for the exercises here.
-}
-
+    function liftOffPushed(){
+        paragraph.innerHTML = "Houston, We Have Liftoff"
+    }
+    function hoverAbort(){
+      missionAbort.style.backgroundColor = 'red'; 
+    }
+    function clickAbort(){
+      response = window.confirm('Are you sure you want to abort the mission?');
+    }
+    button.addEventListener("click", liftOffPushed);
+    missionAbort.addEventListener("mouseover", hoverAbort);
+    missionAbort.addEventListener("click",clickAbort);
+    }
+    
 window.onload = init;
